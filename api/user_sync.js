7fedd136b800a3add5dd76f665315df8ebaaee78
jayradadiya2006 <jayradadiya2006@gmail.com>
2026-08-30 18:51:15 +0530

    Fix API route exports and silence user sync 405 errors

--- api/user_sync.js
+++ api/user_sync.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
+
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
